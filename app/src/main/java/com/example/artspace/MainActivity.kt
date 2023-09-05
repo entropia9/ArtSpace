911f133f6a7abe81f1658d54998a16601d1c81f6
entropia <marta9kut@gmail.com>
2023-09-05 20:47:22 +0200

    added loading images with glide

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,23 +3,25 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -28,6 +30,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.CrossFade
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,30 +55,52 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpaceApp() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        var imageUrl by remember {
+            mutableStateOf("https://cdnb.artstation.com/p/assets/covers/images/003/196/869/small/marta-kut-thumbnail.jpg?1470918530")
+        }
+        var title by remember {
+            mutableStateOf("Biomodem")
+        }
+        var year by remember {
+            mutableStateOf("2016")
+        }
         ArtworkDisplay(
-            painterResource(id = R.drawable.ic_launcher_background),
+            imageUrl,
             modifier = Modifier.weight(0.6f)
         )
         ArtworkData(
-            title = "Biomodem",
-            artist = "entropia",
-            year = "2016",
+            title = title,
+            artist = stringResource(id = R.string.artist_name),
+            year = year,
             Modifier
                 .weight(0.18f)
                 .padding(bottom = 20.dp)
         )
-        ButtonsRow()
+        ButtonsRow(nextFunction = {
+            imageUrl =
+                "https://cdna.artstation.com/p/assets/images/images/001/961/420/small_square/marta-kut-screenshot001.jpg?1455204041"
+            title="Transgenic organism 02"
+            year="2016"
+        })
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun ArtworkDisplay(image: Painter, modifier: Modifier = Modifier) {
+fun ArtworkDisplay(image: String, modifier: Modifier = Modifier) {
     Surface(
         color = MaterialTheme.colorScheme.secondary,
         modifier = modifier.wrapContentSize(),
         shadowElevation = 20.dp
     ) {
-        Image(painter = image, contentDescription = "placeholder", modifier.padding(20.dp))
+        GlideImage(
+            model = image,
+            contentDescription = "placeholder",
+            modifier = modifier
+                .padding(30.dp)
+                .size(250.dp),
+            transition = CrossFade
+        )
     }
 }
 
